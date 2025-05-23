8087a957364155a1bfb45102dab92d29bde3640e
Jared Storm <jeffhilton.code@gmail.com>
2025-05-23 15:04:45 -0600

    frikkin deno

--- src/deno.ts
+++
@@ -1,5 +0,0 @@
-import { readFile, writeFile } from 'node:fs/promises'
-import { dirname, join } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-export { dirname, fileURLToPath, join, readFile, writeFile }
