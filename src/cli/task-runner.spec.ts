897a13e9824c6831b9997ba1ebb8b58c73f13f88
Jared Storm <jeffhilton.code@gmail.com>
2025-06-02 00:06:33 -0600

    Adding reporting

---
+++ src/cli/task-runner.spec.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect, mock, spyOn, beforeEach, afterEach } from 'bun:test';
+import taskRunner from './task-runner.ts';
+
+describe('TaskRunner', () => {
+  beforeEach(() => {
+    mock.module('./resource-loader.ts', () => ({
+      load: mock(() => ({
+        'echo': 'echo Hello, world!',
+      })),
+    }));
+
+    mock.module('./process.ts', () => ({
+      exec: mock(() => 'Hello, world!'),
+      ipc: mock(() => 'Hello, world!'),
+    }));
+  })
+
+  it('should run a task', async () => {
+    const result = await taskRunner.do('echo', []);
+    expect(result).toBe('Hello, world!\n');
+  });
+
+  it('should throw an error if the task is not found', () => {
+    expect(taskRunner.do('not-a-task', [])).rejects.toThrow('Task not-a-task not found');
+  });
+
+  it("throw an error if there are no tasks", () => {
+    mock.module('./resource-loader.ts', () => ({
+      load: mock(() => null),
+    }));
+
+    expect(taskRunner.do('echo', [])).rejects.toThrow('No tasks found');
+  });
+});
