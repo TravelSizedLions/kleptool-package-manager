38b72f4798301f3eaa2316f016113e88c3ca4a9d
Jared Storm <jeffhilton.code@gmail.com>
2025-06-02 21:53:15 -0600

    Oh man, that's so much easier.

--- src/cli/resource-loader.spec.ts
+++ src/cli/resource-loader.spec.ts
@@ -1,8 +1,9 @@
 import { describe, it, expect, mock, afterEach, beforeEach } from 'bun:test';
 import { z } from 'zod';
 import * as resourceLoader from './resource-loader.ts';
-import { $ } from '../testing/mod.ts';
 
+import { $ } from '../testing/mod.ts';
+import kerror from './kerror.ts';
 const injector = $(import.meta)!;
 
 describe('resource-loader', () => {
@@ -24,7 +25,25 @@ describe('resource-loader', () => {
     });
 
     it('should throw an error if the resource does not exist', () => {
-  
+      const resourcePath = 'test.json';
+
+      injector.readFileSync.mock(() => {
+        throw new Error('File not found');
+      });
+
+      const schema = z.object({test: z.string()})
+
+      expect(() => resourceLoader.load(resourcePath, schema)).throws(kerror.Parsing, 'invalid-klep-resource');
+    });
+
+    it('should throw an error if the resource is not valid JSON', () => {
+      const resourcePath = 'test.json';
+
+      injector.readFileSync.mock(() => 'invalid json');
+
+      const schema = z.object({test: z.string()})
+
+      expect(() => resourceLoader.load(resourcePath, schema)).throws(kerror.Parsing, 'invalid-klep-resource');
     });
   });
 });
